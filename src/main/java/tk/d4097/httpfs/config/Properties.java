fd213a8a9745e0950164303901248d180fe7d29f
Sergio Trett <serge3ling@gmail.com>
2020-07-24 22:47:45 +0300

    CORS removed from Properties.java.

--- src/main/java/tk/d4097/httpfs/config/Properties.java
+++ src/main/java/tk/d4097/httpfs/config/Properties.java
@@ -1,22 +1,12 @@
 package tk.d4097.httpfs.config;
 
-import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "http-file-save")
 public class Properties {
-  List<String> allowedOrigins;
   long gridFsSizeStart;
-
-  public List<String> getAllowedOrigins() {
-    return allowedOrigins;
-  }
-
-  public void setAllowedOrigins(List<String> allowedOrigins) {
-    this.allowedOrigins = allowedOrigins;
-  }
 
   public long getGridFsSizeStart() {
     return gridFsSizeStart;
