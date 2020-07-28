26bf8d8b90982611a1bc1a1585f7e22783c31dc6
Sergio Trett <serge3ling@gmail.com>
2020-07-28 22:50:09 +0300

    Cors.java back, .gitignore added.

---
+++ src/main/java/tk/d4097/httpfs/config/Cors.java
@@ -0,0 +1,28 @@
+package tk.d4097.httpfs.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Cors implements WebMvcConfigurer {
+  @NonNull private final Properties properties;
+
+  public Cors(Properties properties) {
+    this.properties = properties;
+  }
+
+  @NonNull
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        String[] origins = properties.getAllowedOrigins().toArray(new String[] {});
+        registry.addMapping("/**").allowedOrigins(origins);
+      }
+    };
+  }
+}
--- src/main/java/tk/d4097/httpfs/config/Properties.java
+++ src/main/java/tk/d4097/httpfs/config/Properties.java
@@ -3,10 +3,13 @@ package tk.d4097.httpfs.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 @ConfigurationProperties(prefix = "http-file-save")
 public class Properties {
   long gridFsSizeStart;
+  List<String> allowedOrigins;
 
   public long getGridFsSizeStart() {
     return gridFsSizeStart;
@@ -14,5 +17,13 @@ public class Properties {
 
   public void setGridFsSizeStart(long gridFsSizeStart) {
     this.gridFsSizeStart = gridFsSizeStart;
+  }
+
+  public List<String> getAllowedOrigins() {
+    return allowedOrigins;
+  }
+
+  public void setAllowedOrigins(List<String> allowedOrigins) {
+    this.allowedOrigins = allowedOrigins;
   }
 }
