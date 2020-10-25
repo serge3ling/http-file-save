7e3122b8cbcf7dd47653c96cb461c59423b66897
Sergio Trett <serge3ling@gmail.com>
2020-10-25 22:04:07 +0200

    FileModel interface methods added.

--- src/main/java/tk/d4097/httpfs/service/FileModel.java
+++ src/main/java/tk/d4097/httpfs/service/FileModel.java
@@ -1,4 +1,8 @@
 package tk.d4097.httpfs.service;
 
 public interface FileModel { // getters for id, size, name, description?
+  String getId();
+  long getSize();
+  String getName();
+  String getDescription();
 }
